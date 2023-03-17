054ed40aa8e387194416eae8b30f2294d42e86fe
Mustafa Hussein <mustafahussein.a@gmail.com>
2023-03-16 22:03:50 -0400

    Review Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+//function multiply(num1, num2)
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+//console.log(Math.random());
+
+let num3 = 18;
+let num4 = 7;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(5, 10, 15, 20);
+console.log(max);
